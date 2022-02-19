3864f938fb864594b79b4e1e5eb5f13bdfe73c99
32BITAcademy <32bitacademy@gmail.com>
2022-02-19 20:31:03 +0300

    Добвлен файл Header, хитрая функция с ответом пользователю

---
+++ 20IVT_GIT/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+
+void func(int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("Hi! ");
+	printf("\n");
+}
--- 20IVT_GIT/Source.cpp
+++ 20IVT_GIT/Source.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include "Header.h"
 
 int main()
 {
 	int a;
 	scanf_s("%i", &a);
-	printf("You wrote %i\n", a);
+	printf("You wrote %i, I answer:", a);
+	func(a);
 
 	return 0;
 }
